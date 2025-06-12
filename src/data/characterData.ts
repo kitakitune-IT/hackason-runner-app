646db3881541a851c08cfa193241bd650536a673
Yuto-ecc <2250008@ecc.ac.jp>
2025-06-12 21:20:29 +0900

    著作権対策

--- src/data/characterData.ts
+++ src/data/characterData.ts
@@ -5,7 +5,7 @@ import emuCharacter from '../assets/emu-14760_256.gif';
 import horseCharacter from '../assets/horse_256.gif';
 import gundumCharacter from '../assets/オルガ・イツカ.gif';
 import racoonCharacter from '../assets/たぬき.gif';
-import pocketCharacter from '../assets/ピカチュウ-透過.gif';
+import pocketCharacter from '../assets/でんきねずみ.gif';
 
 // 2. キャラクターの型を定義する
 export interface Character {
@@ -21,8 +21,8 @@ export const availableCharacters: Character[] = [
   { id: 2, name: '猫っぽいの', imageSrc: catlikeCharacter, price: 0 },
   { id: 3, name: 'エミュー', imageSrc: emuCharacter, price: 0 },
   { id: 4, name: '馬', imageSrc: horseCharacter, price: 0 },
-  { id: 5, name: 'オルガ・イツカ', imageSrc: gundumCharacter, price: 100},
+  { id: 5, name: '〇ガ・イツカ', imageSrc: gundumCharacter, price: 100},
   { id: 6, name: 'たぬき', imageSrc: racoonCharacter, price: 0},
-  { id: 7, name: 'ピカチュウ', imageSrc: pocketCharacter, price: 0}
+  { id: 7, name: 'でんきねずみ', imageSrc: pocketCharacter, price: 0}
   // ...もし画像が増えたら、ここに追加していく
 ];
